b02d0f8d2bc8a9102105e62cba49733fc2e106f9
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 01:38:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.collect do |guests|
+    badge_maker(guests)
+  end
+end
+
+def assign_rooms
+
+end
