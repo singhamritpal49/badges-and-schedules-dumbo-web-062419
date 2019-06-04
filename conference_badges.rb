abc9ef5e2c11da128d849201890e343d4de41bbc
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 01:59:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,5 +9,9 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(names)
-  names.each_with_index {|name,index| puts "Hello, #{name}! You'll be assigned to room #{index += 1}!" }
+  new_array= []
+  names.each_with_index {|name,index|
+    #puts "Hello, #{name}! You'll be assigned to room #{index += 1}!"
+    new_array.push(puts "Hello, #{name}! You'll be assigned to room #{index += 1}!")
+  }
 end
