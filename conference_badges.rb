cf0dcdb5b3abe75f3c7be8f7cc9da5a4a56d6840
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-04 02:24:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,9 @@ def assign_rooms(names)
   new_array = []
   names.each_with_index {|i,index|
     #new_array.push(i)
-    puts "Hello, #{i}! You'll be assigned to room #{index += 1}!"
+    #puts "Hello, #{i}! You'll be assigned to room #{index += 1}!"
+    new_array.push(puts "Hello, #{i}! You'll be assigned to room #{index += 1}!")
+
     #return new_array
      #new_array.push(i)
 
